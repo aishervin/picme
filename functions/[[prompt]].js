4c1ea49414adee104a15cf57f136c8b935975c7f
SHΞЯVIN™ <cyphergeminis@gmail.com>
2026-08-06 07:54:19 +0330

    ☬Exclusive SHΞN™ made

--- functions/[[prompt]].js
+++ functions/[[prompt]].js
@@ -5,20 +5,29 @@ export async function onRequest(context) {
 
   if (!prompt) {
     return new Response(
-  `<html><body style="font-family:system-ui;margin:0;padding:1rem;min-height:100vh;display:flex;flex-direction:column;justify-content:center;align-items:center;background-color:#121212;color:#ffffff;box-sizing:border-box;">
-   <div style="text-align:center;">
-     <h2 style="margin-bottom:0.5rem;">PIC🎴SHΞN™ </h2>
-     <p style="color:#b0b0b0;">
-       Add prompt after URL, e.g.<br>
-       <a href="/SHERVIN%20logotype" 
-          style="color:#00e5ff;text-decoration:underline;text-underline-offset:2px;">
-         Picshen.page.dev/SHERVIN Logotype
-       </a>
-     </p>
-   </div>
-   </body></html>`,
-  { headers: { "content-type": "text/html;charset=UTF-8" } }
-);
+      `<html>
+        <body style="font-family:system-ui;margin:0;padding:1rem;min-height:100vh;display:flex;flex-direction:column;justify-content:center;align-items:center;background-color:#121212;color:#ffffff;box-sizing:border-box;">
+          <div style="text-align:center;">
+            <h2 style="margin-bottom:0.5rem;">PIC🎴SHΞN™</h2>
+            <p style="color:#b0b0b0;">
+              Add prompt after URL, e.g.<br>
+              <a href="/SHERVIN%20logotype"
+                 style="color:#00e5ff;text-decoration:underline;text-underline-offset:2px;">
+                Picshen.page.dev/SHERVIN Logotype
+              </a>
+            </p>
+          </div>
+        </body>
+      </html>`,
+      { headers: { "content-type": "text/html;charset=UTF-8" } }
+    );
+  }
+
+  try {
+    const response = await env.AI.run(
+      "@cf/black-forest-labs/flux-1-schnell",
+      { prompt }
+    );
 
     // تبدیل داده base64 به بایت‌های تصویر
     const binaryString = atob(response.image);
